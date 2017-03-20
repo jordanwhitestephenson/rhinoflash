2d9150f5b8a7161d7eab769e7fe92e969e67d4e0
Jordan Stephenson <jordanwhitestephenson@gmail.com>
2017-03-20 16:27:47 -0600

    working on user dashbaord

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,32 +1,40 @@
+
 $(document).ready( function() {
 
+
   $('body').on("click", ".larg div h3", function(){
-    if ($(this).children('span').hasClass('close')) {
-      $(this).children('span').removeClass('close');
-    }
-    else {
-      $(this).children('span').addClass('close');
-    }
-    $(this).parent().children('p').slideToggle(250);
+    $(this).parent().children('p').slideToggle(250)
   });
 
-  $('body').on("click", "nav ul li a", function(){
+  var badgeimage =
+  $('#member').on("click", "a", function(){
+    $.ajax({
+        method: 'GET',
+        url: 'https://flash-backend.herokuapp.com/username',
+        contentType: "application/json",
+        data: JSON.stringify()
+    }).then(function(users) {
+        console.log(users)
+      $('.div1 h3').text('Welcome : ' + users[0].name + ' !');
+    });
+
+
     var title = $(this).data('title');
+    var userBadges = $(this).data('userbadge')
     var body = $(this).data('body')
     var heading = $(this).data('heading')
     var about = $(this).data('about')
     var userbio = $(this).data('userbio')
-    var makeCards = $(this).data('makecards')
     var userStats = $(this).data('userstats')
     var statOverview = $(this).data('statoverview')
 
-    $('.title').children('h2').html(title);
+    $('.title').children('h2').append(title);
     $('.div1').children('p').html(body);
     $('.div1').children('h3').html(heading);
     $('.div2').children('h3').html(about);
     $('.div2').children('p').html(userbio);
-    $('.div2').children('p').html(makeCards);
-    $('.div3').children('h3').html(statOverview)
     $('.div3').children('p').html(userStats)
-  });
+    $('.div3').children('h3').html(statOverview)
+    $('.imageDiv').children('p').html('<img src =' + userBadges + '>')
+  })
 });
