c0082a4abc9500ac714d7fb061f5111055b5fe8f
Jordan Stephenson <jordanwhitestephenson@gmail.com>
2017-03-22 13:12:59 -0600

    worked on login

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,6 @@
-$('.login-form__submit').on('click', function(e){
-  e.preventDefault();
-  console.log('it worls')
- window.location =  `/dashboard.html?email=${'Jeff@gmail.com'}`
-
-})
+// $('.login-form__submit').on('click', function(e){
+//   e.preventDefault();
+//   console.log('it worls')
+//  window.location =  `/dashboard.html?email=${'Jeff@gmail.com'}`
+//
+// })
