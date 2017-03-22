ea927afb09f679f33d34de1a4d5fbd4b2686a4e0
Sara <seking13@gmail.com>
2017-03-22 12:25:50 -0600

    fields made makeDeck

--- public/js/makeDeck.js
+++ public/js/makeDeck.js
@@ -20,15 +20,20 @@ $(document).ready(function() {
 });
 
 
-
 /*global $*/
 
+
+$( "form" ).on( "submit", function( event ) {
+  event.preventDefault();
+
+  console.log( $( this ).serialize() );
+});
+
 let cardPost = {};
-cardPost['email'] = 'Jordan@gmail.com';
+cardPost['email'] = myEmail;
 let subjectPost = {};
 
 function appendSubject() {
-  console.log('hello');
   $.ajax({
     method: 'GET',
     url: 'https://rhinocards.herokuapp.com/subject',
@@ -36,15 +41,16 @@ function appendSubject() {
     data: JSON.stringify()
   }).then(function (subject) {
     console.log(subject);
-    console.log('hellejflakj');
     for (var i = 0; i < subject.length; i++) {
       $('.subjectSelection')
-        .append(`<li class="dropval" id="${subject[i].name}"><a>${subject[i].name}</a></li>`);
+        .append(`<option class="dropval" id="${subject[i].name}">${subject[i].name}</option>`);
     }
   })
 }
+
 $(document).ready(function () {
   appendSubject()
+  $('select').material_select();
 });
 
 // var selected = document.getElementById('dropdown1')
@@ -57,12 +63,12 @@ $(document).on('click', '.dropval', function () {
   console.log("cardPost", cardPost);
   console.log(id)
 });
-
-$("#addCard").click(function () {
-  front
-  back
-  deck_id
-});
+//
+// $("#addCard").click(function () {
+//   front
+//   back
+//   deck_id
+// });
 
 // name
 // subject
@@ -86,8 +92,8 @@ $("#addDeck").click(function () {
 });
 
 
-// Card: {"front":"history1 front","back":"history1 back","deck_id":1}
+// flashcard: "id":1,"front":"history1 front","back":"history1 back","deck_id":1},
 
-// Deck: {"subject":"History","email":"Jeff@gmail.com","front":"history9 front","back":"history9 back","name":"history101","id":1}
+// Deck: [{"subject":"History","email":"Jeff@gmail.com","front":"history9 front","back":"history9 back","name":"history101","id":1},{"subject":"History","email":"Jeff@gmail.com","front":"history8 front","back":"history8 back","name":"history101","id":1},{"subject":"History","email":"Jeff@gmail.com","front":"history7 front","back":"history7 back","name":"history101","id":1},{"subject":"History","email":"Jeff@gmail.com","front":"history6 front","back":"history6 back","name":"history101","id":1},{"subject":"History","email":"Jeff@gmail.com","front":"history5 front","back":"history5 back","name":"history101","id":1},{"subject":"History","email":"Jeff@gmail.com","front":"history4 front","back":"history4 back","name":"history101","id":1},{"subject":"History","email":"Jeff@gmail.com","front":"history3 front","back":"history3 back","name":"history101","id":1},{"subject":"History","email":"Jeff@gmail.com","front":"history2 front","back":"history2 back","name":"history101","id":1},{"subject":"History","email":"Jeff@gmail.com","front":"history1 front","back":"history1 back","name":"history101","id":1}
 
 // badge, subject, username, deck, favorite, flashcard
