112543df29cf5841f935892a9ec2e6d96b98c731
Jeff Miguel Hernandez <jeffhernandez89@gmail.com>
2017-03-22 17:38:09 -0600

    play is kind of working

--- public/js/play.js
+++ public/js/play.js
@@ -5,62 +5,65 @@ console.log(deckID)
 let submitCase = false;
 let answer;
 let back;
-let checkmark = `
-<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-         viewBox="0 0 98.5 98.5" enable-background="new 0 0 98.5 98.5" xml:space="preserve">
-        <path class="checkmark" fill="none" stroke-width="8" stroke-miterlimit="10" d="M81.7,17.8C73.5,9.3,62,4,49.2,4
-        C24.3,4,4,24.3,4,49.2s20.3,45.2,45.2,45.2s45.2-20.3,45.2-45.2c0-8.6-2.4-16.6-6.5-23.4l0,0L45.6,68.2L24.7,47.3"/>
-      </svg>
-`
+let globalDeck;
+let count = 0;
+let correct = [];
+let cardAnswers = [];
+const rightAnswer = `<img src="img/right.png" alt="checkmark" class="rightWrong">`;
+const wrongAnswer = `<img src="img/wrong.png" alt="checkmark" class="rightWrong">`;
+const rightAnswerSmall = `<img src="img/right.png" alt="checkmark" class="rightWrong" style="width:70px;height:auto;">`;
+const wrongAnswerSmall = `<img src="img/wrong.png" alt="checkmark" class="rightWrong" style="width:70px;height:auto;">`;
 
-$(document).ready(function() {
 
-    $('#showDeckClass').on('click', function() {
-        window.location = `/showDecks.html?email=${myEmail}`
-    });
-    $('#showStudyClass').on('click', function() {
-        window.location = `/study.html?email=${myEmail}`
-    });
-    $('#showPlayClass').on('click', function() {
-        window.location = `/play.html?email=${myEmail}`
-    });
-    $('#showDashboardClass').on('click', function() {
-        window.location = `/dashboard.html?email=${myEmail}`
-    });
+$(document).ready(function () {
 
-    $(".mat-input").focus(function(){
-      $(this).parent().addClass("is-active is-completed");
-    });
+  $('#showDeckClass').on('click', function () {
+    window.location = `/showDecks.html?email=${myEmail}`
+  });
+  $('#showStudyClass').on('click', function () {
+    window.location = `/study.html?email=${myEmail}`
+  });
+  $('#showPlayClass').on('click', function () {
+    window.location = `/play.html?email=${myEmail}`
+  });
+  $('#showDashboardClass').on('click', function () {
+    window.location = `/dashboard.html?email=${myEmail}`
+  });
 
-    $(".mat-input").focusout(function(){
-      if($(this).val() === "")
-        $(this).parent().removeClass("is-completed");
-      $(this).parent().removeClass("is-active");
-    })
+  $(".mat-input").focus(function () {
+    $(this).parent().addClass("is-active is-completed");
+  });
 
-    $.ajax({
-        method: 'GET',
-        url: `https://rhinocards.herokuapp.com/deck/${deckID}`,
-        contentType: "application/json",
-        data: JSON.stringify()
-    }).then(function(deck) {
-      back = deck[0].back;
-      console.log(deck[0]);
-        $('.deckTitleHere').append(`<center><h2> Deck Name : ${deck[0].deck_name} </h2></center>`)
-            $('.appendHere').append(`<div class="section no-pad-bot" id="index-banner">
+  $(".mat-input").focusout(function () {
+    if ($(this).val() === "")
+      $(this).parent().removeClass("is-completed");
+    $(this).parent().removeClass("is-active");
+  })
+
+  $.ajax({
+    method: 'GET',
+    url: `https://rhinocards.herokuapp.com/deck/${deckID}`,
+    contentType: "application/json",
+    data: JSON.stringify()
+  }).then(function (deck) {
+    globalDeck = deck;
+    back = deck[0].back;
+    console.log(deck[0]);
+    $('.deckTitleHere').append(`<center><h2> Deck Name : ${deck[0].deck_name} </h2></center>`)
+    $('.appendHere').append(`<div class="section no-pad-bot" id="index-banner">
             <div class="container">
                 <br><br>
                 <div id="wrap">
                     <div class="container">
                         <div class="row" id="midrow">
-                            <div class="flip-container" id="flashcard">
+                            <div class="flip-container " id="flashcard">
                                 <div class="flipper">
                                     <div class="front">
-                                        <span id="flashcard--content_en">${deck[0].front}</span>
+                                        <span id="flashcard--content_en">${deck[count].front}</span>
                                         <button class="flag"><i class="fa fa-flag" aria-hidden="true"></i></button>
                                     </div>` +
-                                    `<div class="back">
-                                        <span id="flashcard--content_es">${deck[0].back}</span>
+      `<div class="back">
+                                        <span id="flashcard--content_es">${deck[count].back}</span>
                                     </div>
                                 </div>
                             </div>
@@ -69,30 +72,108 @@ $(document).ready(function() {
                 </div>
             </div>
         </div>`)
-    });
-    // <--CARD FLIPPER-->
-    var flashcard = document.getElementById('flashcard');
-    var refreshBtns = document.getElementsByClassName('refresh');
+  });
 
-    function checkAnswer(answer) {
-      if (answer.toUpperCase() === back.toUpperCase()) {
-        $('.wrapper').append(checkmark)
+  function finishedDeck () {
+    $('.appendHere').empty();
+    $('.answerLine').empty();
+    globalDeck.forEach(function(card, i) {
+      var distinguishCorrect;
+      if (correct[i]) {
+        distinguishCorrect = rightAnswerSmall;
       } else {
-        console.log('DUMBASS');
+        distinguishCorrect = wrongAnswerSmall;
       }
-    }
+        $('.deckTitleHere').append(`<div class="section no-pad-bot" id="index-banner">
+        <div class="container">
+            <br><br>
+            <div id="wrap">
+                <div class="container">
+                    <div class="row" id="midrow">
+                        <div class="flip-container" id="flashcard">
+                            <div class="flipper">
+                                <div class="front">
+                                <div class="topCorner">
+                                ${distinguishCorrect}
+                                </div>
+                                    <span id="flashcard--content_en">${card.front}</span>
+                                    <button class="flag"><i class="fa fa-flag" aria-hidden="true"></i></button>
+                                </div>` +
+                                `<div class="back">
+                                <div class="topCorner">
+                                ${distinguishCorrect}
+                                </div>
+                                    <span id="flashcard--content_es">${card.back}</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="userAnswer">
+                      <span id="asljdf">Your Answer:&nbsp</span>
+                      <span id="flashcard--content_es">${cardAnswers[i]}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>`)
+    });
+    console.log(correct);
+  }
 
-    $(document).on('click', '.submitButton', function(event) {
-        event.preventDefault();
-        answer = $('.answerInput').val()
-        checkAnswer(answer);
-        $('.flip-container').addClass('flipped');
-        submitCase = true;
-    });
-    $(document).on('click', '#flashcard', function(event) {
-      event.preventDefault();
-      if (submitCase){
-        this.classList.toggle('flipped');
-      }
-    });
+  $(document).on('click', '.nextQuestion', function (event) {
+    event.preventDefault();
+    if (count < globalDeck.length) {
+      submitCase = false;
+      $('.wrapper').empty()
+      document.getElementById("flashcard--content_en").innerHTML = globalDeck[count].front;
+      document.getElementById("flashcard--content_es").innerHTML = globalDeck[count].back;
+      $('.nextQuestion').text('submit')
+      $('.nextQuestion').addClass('submitButton')
+      $('.submitButton').removeClass('nextQuestion')
+      $('.flip-container').removeClass('flipped');
+      $('.answerInput').val('')
+    } else {
+      finishedDeck()
+    }
+  });
+
+  var flashcard = document.getElementById('flashcard');
+  var refreshBtns = document.getElementsByClassName('refresh');
+
+  function checkAnswer(answer) {
+    cardAnswers.push(answer)
+    if (answer.toUpperCase() === globalDeck[count].back.toUpperCase()) {
+      correct.push(1);
+      $('.wrapper').append(rightAnswer);
+    } else {
+      correct.push(0);
+      $('.wrapper').append(wrongAnswer);
+    }
+    count++;
+    $('.submitButton').text('Next question')
+    $('.submitButton').addClass('nextQuestion')
+    $('.nextQuestion').removeClass('submitButton')
+  }
+
+  $(document).keypress(function (event) {
+    if (event.keyCode == 13) {
+      $(".universalButton").click();
+    }
+  });
+
+  $(document).on('click', '.submitButton', function (event) {
+    event.preventDefault();
+    if (!submitCase) {
+      answer = $('.answerInput').val()
+      checkAnswer(answer);
+      $('.flip-container').addClass('flipped');
+      submitCase = true;
+    }
+  });
+  $(document).on('click', '#flashcard', function (event) {
+    event.preventDefault();
+    if (submitCase) {
+      this.classList.toggle('flipped');
+    }
+  });
 });
