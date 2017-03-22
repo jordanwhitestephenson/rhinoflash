e0a96b62b348169ade05ff0e934d6596dcca91dc
Jeff Miguel Hernandez <jeffhernandez89@gmail.com>
2017-03-22 13:03:17 -0600

    play things

--- public/js/play.js
+++ public/js/play.js
@@ -3,6 +3,16 @@ console.log(myEmail);
 var deckID = decodeURIComponent(window.location.search).split("=")[2];
 console.log(deckID)
 let submitCase = false;
+let answer;
+let back;
+let checkmark = `
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+         viewBox="0 0 98.5 98.5" enable-background="new 0 0 98.5 98.5" xml:space="preserve">
+        <path class="checkmark" fill="none" stroke-width="8" stroke-miterlimit="10" d="M81.7,17.8C73.5,9.3,62,4,49.2,4
+        C24.3,4,4,24.3,4,49.2s20.3,45.2,45.2,45.2s45.2-20.3,45.2-45.2c0-8.6-2.4-16.6-6.5-23.4l0,0L45.6,68.2L24.7,47.3"/>
+      </svg>
+`
+
 $(document).ready(function() {
 
     $('#showDeckClass').on('click', function() {
@@ -34,6 +44,7 @@ $(document).ready(function() {
         contentType: "application/json",
         data: JSON.stringify()
     }).then(function(deck) {
+      back = deck[0].back;
       console.log(deck[0]);
         $('.deckTitleHere').append(`<center><h2> Deck Name : ${deck[0].deck_name} </h2></center>`)
             $('.appendHere').append(`<div class="section no-pad-bot" id="index-banner">
@@ -63,8 +74,18 @@ $(document).ready(function() {
     var flashcard = document.getElementById('flashcard');
     var refreshBtns = document.getElementsByClassName('refresh');
 
+    function checkAnswer(answer) {
+      if (answer.toUpperCase() === back.toUpperCase()) {
+        $('.wrapper').append(checkmark)
+      } else {
+        console.log('DUMBASS');
+      }
+    }
+
     $(document).on('click', '.submitButton', function(event) {
         event.preventDefault();
+        answer = $('.answerInput').val()
+        checkAnswer(answer);
         $('.flip-container').addClass('flipped');
         submitCase = true;
     });
