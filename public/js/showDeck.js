4ed7143ca1977f8c9f632072cda459162178d5d2
Jordan Stephenson <jordanwhitestephenson@gmail.com>
2017-03-23 08:26:02 -0600

    redesigned dashboard

--- public/js/showDeck.js
+++ public/js/showDeck.js
@@ -2,18 +2,18 @@ var myEmail = decodeURIComponent(window.location.search).split("=")[1];
 console.log(myEmail);
 
 $(document).ready(function() {
-    $('#showDeckClass').on('click', function() {
-        window.location = `/showDecks.html?email=${myEmail}`
-    });
-    $('#showStudyClass').on('click', function() {
-        window.location = `/study.html?email=${myEmail}`
-    });
-    $('#showPlayClass').on('click', function() {
-        window.location = `/play.html?email=${myEmail}`
-    });
-    $('#showDashboardClass').on('click', function() {
-        window.location = `/dashboard.html?email=${myEmail}`
-    });
+  $('.showDeckClass').on('click', function() {
+      window.location = `/showDecks.html?email=${myEmail}`
+  });
+  $('.showStudyClass').on('click', function() {
+      window.location = `/study.html?email=${myEmail}`
+  });
+  $('.showPlayClass').on('click', function() {
+      window.location = `/play.html?email=${myEmail}`
+  });
+  $('.showDashboardClass').on('click', function() {
+      window.location = `/dashboard.html?email=${myEmail}`
+  });
 
 
 
@@ -24,77 +24,80 @@ $(document).ready(function() {
         data: JSON.stringify()
     }).then(function(user) {
         var unique = _.uniqBy(user, 'deck_name');
-        console.log(user)
         var pluck = unique.map(function(deck) {
             return _.get(deck, 'deck_name');
         });
         unique.forEach(function(deck) {
-            console.log(unique)
             $('.col').append(`<div class="flip-container" id="flashcard" style="margin: 20px;"><div class="front"><div class="flipper"></div>
                     <span id="flashcard--content_en"><a href="${deck.deck_id}"></a>${deck.deck_name}</span>
                   </div>
                   </div>
+
                   <div class = "deckButtons">
-                  <a class="waves-effect waves-light btn-large" id= ${deck.deck_id} href="play.html?email=${myEmail}?deck_id=${deck.deck_id}">Play</a>` +
+                  <a class="waves-effect waves-light btn" id= ${deck.deck_id} href="play.html?email=${myEmail}?deck_id=${deck.deck_id}">Play</a>` +
 
-                  `<a class="btn-large unFavorite" id= ${deck.deck_id} href="">Favorite</a>` +
+                `<a class="btn unFavorite" id= ${deck.deck_id} href="">Favorite</a>` +
 
-                  `<a class="waves-effect waves-light btn-large" id= ${deck.deck_id} href="/study.html?email=${myEmail}?deck_id=${deck.deck_id}">Study</a>
+                `<a class="btn deleteButton" id= ${deck.deck_id} href="#"> Delete</a>`
+                   +
+
+                `<a class="waves-effect waves-light btn" id= ${deck.deck_id} href="/study.html?email=${myEmail}?deck_id=${deck.deck_id}">Study</a>
                   </div></div>`)
         });
 
-        $('.unFavorite').on('click', function(event) {
+        $(".unFavorite").click(function(event) {
             event.preventDefault();
-            // var favoriteDeck = $(this).attr('id');
+            $(this).toggleClass("favorite")
 
-              $(this).toggleClass("favorite")
+            if ($(this).hasClass("favorite")) {
+                var favoritedDeckID = $(this).attr('id');
+                console.log(this)
+                console.log('favorited', favoritedDeckID)
 
-
-            //     'background-color': 'yellow',
-            //     'width': '50px',
-            //     'height': '40px',
-            //     'font-size': '8px',
-            //     'color': '#000',
-            //     'padding-right': '80px',
-            //     'font-weight': '600',
-            //     'padding-bottom': '50px'
-            // }).text('Favorited!')
-
-            var favoriteDecks = {
-                email: `${myEmail}`,
-                id: `${favoriteDeck}`
-            };
-        //     $.ajax({
-        //             method: "POST",
-        //             url: "https://rhinocards.herokuapp.com/favorite",
-        //             data: JSON.stringify(favoriteDecks),
-        //             contentType: "application/json"
-        //         })
-        //         .then(response => {
-        //             function changeClass() {
-        //                 $('.waves-effect.waves-light.btn-large.favorite').removeClass('favorite');
-        //                 $(this).addClass('newFavorite')
-        //                 event.preventDefault();
-        //             }
-        //             $('.favorite').on('click', changeClass);
-        //         });
-        //         if ($('.waves-effect.waves-light.btn-large').hasClass('.newFavorite')){
-        //           ('.newFavorite').text('YA!')
-        //           var unfavoriteDeck = $(this).attr('id');
-        //           var unfavoriteDecks = {
-        //               email: `${myEmail}`,
-        //               id: `${unfavoriteDeck}`
-        //           };
-        //           $.ajax({
-        //                   method: "DELETE",
-        //                   url: "https://rhinocards.herokuapp.com/favorite",
-        //                   data: JSON.stringify(unfavoriteDecks),
-        //                   contentType: "application/json"
-        //               }).then(response => {
-        //                 console.log('unfavorited', response)
-        //               })
-        //
-        //         }
+                var favoriteDecks = {
+                    email: `${myEmail}`,
+                    id: `${favoritedDeckID}`
+                };
+                $.ajax({
+                    method: "POST",
+                    url: "https://rhinocards.herokuapp.com/favorite",
+                    data: JSON.stringify(favoriteDecks),
+                    contentType: "application/json"
+                }).then(response => {
+                    console.log('success')
+                })
+            } else if ($(this).hasClass("unFavorite")) {
+                var unfavoritedDeckID = $(this).attr('id');
+                console.log(this)
+                console.log('unfavorited', unfavoritedDeckID)
+                var unfavoriteDecks = {
+                    email: `${myEmail}`,
+                    id: `${unfavoritedDeckID}`
+                };
+                $.ajax({
+                    method: "DELETE",
+                    url: "https://rhinocards.herokuapp.com/favorite",
+                    data: JSON.stringify(unfavoriteDecks),
+                    contentType: "application/json"
+                }).then(response => {
+                    console.log('deleted', unfavoriteDecks);
+                });
+            }
         });
+        $('.deleteButton').on('click',function(event){
+          event.preventDefault();
+          var deleteID = $(this).attr('id');
+          console.log(deleteID)
+          console.log('YA!')
+          $.ajax({
+              method: "DELETE",
+              url: `https://rhinocards.herokuapp.com/username/${myEmail}/${deleteID}`,
+              data: JSON.stringify(unfavoriteDecks),
+              contentType: "application/json"
+          }).then(response => {
+              console.log('deleted', unfavoriteDecks);
+          });
+
+        })
     });
 });
