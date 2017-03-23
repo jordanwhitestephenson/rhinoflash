be3ce91fdb62fce2118bb700e9db84c5d9a4e6fc
BRAD FOLKERS <BRADLEYDFOLKERS@GMAIL.COM>
2017-03-23 11:13:42 -0600

    Make deck page finished

--- public/js/makeDeck.js
+++ public/js/makeDeck.js
@@ -1,94 +1,134 @@
-var myEmail = decodeURIComponent(window.location.search).split("=")[1];
-console.log(myEmail);
+$(function() {
+  var myEmail = decodeURIComponent(window.location.search).split("=")[1];
+  console.log(myEmail);
+  var cardCount = 1;
+  appendSubject()
+  $('.showDeckClass').on('click', function() {
+      window.location = `/showDecks.html?email=${myEmail}`
+  });
+  $('.showStudyClass').on('click', function() {
+      window.location = `/study.html?email=${myEmail}`
+  });
+  $('.showPlayClass').on('click', function() {
+      window.location = `/play.html?email=${myEmail}`
+  });
+  $('.showDashboardClass').on('click', function() {
+      window.location = `/dashboard.html?email=${myEmail}`
+  });
 
-$(document).ready(function() {
-    appendSubject()
-    $('.showDeckClass').on('click', function() {
-        window.location = `/showDecks.html?email=${myEmail}`
-    });
-    $('.showStudyClass').on('click', function() {
-        window.location = `/study.html?email=${myEmail}`
-    });
-    $('.showPlayClass').on('click', function() {
-        window.location = `/play.html?email=${myEmail}`
-    });
-    $('.showDashboardClass').on('click', function() {
-        window.location = `/dashboard.html?email=${myEmail}`
-    });
-
-});
-
-
-/*global $*/
-
-
-$("form").on("submit", function(event) {
+  $('.addCard').on('click', function(event) {
     event.preventDefault();
-    console.log($(this));
-    console.log($('#NameOfDeck').val())
-    var myNewDeck = {
-        subject: $('#Subject').val(),
-        email: `${myEmail}`,
-        front: $('#Question').val(),
-        back: $('#Answer').val(),
-        name: $('#NameOfDeck').val()
+    ++cardCount;
+    $('.card-section').append(`
+      <div class="input-field col s6">
+          <input id="Question-${cardCount}" type="text" class="validate">
+          <label>Question</label>
+      </div>
+      <div class="input-field col s6">
+          <input id="Answer-${cardCount}" type="text" class="validate">
+          <label>Answer</label>
+      </div>
+      `)
+  })
+
+  $('.submit-deck').on('click', function(event) {
+    event.preventDefault();
+    var deckObject = {};
+    deckObject.name = $('#NameOfDeck').val();
+    deckObject.email = myEmail;
+    deckObject.subject = $('#Subject option:selected').val();
+
+    $.post('https://rhinocards.herokuapp.com/deck', deckObject)
+      .then(function(data, status) {
+        console.log(data);
+      })
+      .catch(function(error) {
+        console.log(error);
+      })
+
+    var subject = deckObject.subject;
+    for (let i = 0; i < cardCount; i++) {
+      var cardFront = $('#Question-' + (i + 1)).val();
+      var cardBack = $('#Answer-' + (i + 1)).val();
+      var cardObject = {front: cardFront, back: cardBack, name: subject};
+      $.post('https://rhinocards.herokuapp.com/flashcard', cardObject)
+        .then(function(data, status) {
+          console.log(data);
+        })
+        .catch(function(error) {
+          console.log(error);
+        })
     }
-    $.ajax({
-        method: "POST",
-        url: "https://rhinocards.herokuapp.com/deck",
-        data: JSON.stringify(myNewDeck),
-        contentType: "application/json"
-    }).then(response => {
-        console.log(response)
-    })
-});
+
+  })
+
+  // $("form").on("submit", function(event) {
+  //     event.preventDefault();
+  //     console.log($(this));
+  //     console.log($('#NameOfDeck').val())
+  //     var myNewDeck = {
+  //         subject: $('#Subject').val(),
+  //         email: `${myEmail}`,
+  //         front: $('#Question').val(),
+  //         back: $('#Answer').val(),
+  //         name: $('#NameOfDeck').val()
+  //     }
+  //     $.ajax({
+  //         method: "POST",
+  //         url: "https://rhinocards.herokuapp.com/deck",
+  //         data: JSON.stringify(myNewDeck),
+  //         contentType: "application/json"
+  //     }).then(response => {
+  //         console.log(response)
+  //     })
+  // });
 
 
-let cardPost = {};
-cardPost['email'] = myEmail;
-let subjectPost = {};
+  let cardPost = {};
+  cardPost['email'] = myEmail;
+  let subjectPost = {};
 
-function appendSubject() {
-    $.ajax({
-        method: 'GET',
-        url: 'https://rhinocards.herokuapp.com/subject',
-        contentType: 'application/json',
-        data: JSON.stringify()
-    }).then(function(subject) {
-        console.log(subject);
-        for (var i = 0; i < subject.length; i++) {
-            $('.subjectSelection')
-                .append(`<option class="dropval" id="${subject[i].name}">${subject[i].name}</option>`);
-        }
-    })
-}
+  function appendSubject() {
+      $.ajax({
+          method: 'GET',
+          url: 'https://rhinocards.herokuapp.com/subject',
+          contentType: 'application/json',
+          data: JSON.stringify()
+      }).then(function(subject) {
+          for (var i = 0; i < subject.length; i++) {
+              $('.subjectSelection')
+                  .append(`<option class="dropval" id="${subject[i].name}">${subject[i].name}</option>`);
+          }
+      })
+  }
 
-$(document).ready(function() {
-    appendSubject()
-    $('select').material_select();
-});
+  // $(document).ready(function() {
+  //     appendSubject()
+  //     $('select').material_select();
+  // });
 
-$(document).on('click', '.dropval', function() {
-    var id = $(this).text();
-    cardPost['name'] = id;
-    console.log("cardPost", cardPost);
-    console.log(id)
-});
-
+  // $(document).on('click', '.dropval', function() {
+  //     var id = $(this).text();
+  //     cardPost['name'] = id;
+  //     console.log("cardPost", cardPost);
+  //     console.log(id)
+  // });
 
 
-$("#addDeck").click(function() {
-    $.ajax({
-            url: 'https://rhinocards.herokuapp.com/deck',
-            method: 'POST',
-            data: JSON.stringify(cardPost),
-            contentType: 'application/json; charset=utf-8',
 
-        })
-        .then((response) => {
-            console.log('response', response);
-        })
-        .catch((response) => {
-            console.log('error');
-        });
-});
+  // $("#addDeck").click(function() {
+  //     $.ajax({
+  //             url: 'https://rhinocards.herokuapp.com/deck',
+  //             method: 'POST',
+  //             data: JSON.stringify(cardPost),
+  //             contentType: 'application/json; charset=utf-8',
+  //
+  //         })
+  //         .then((response) => {
+  //             console.log('response', response);
+  //         })
+  //         .catch((response) => {
+  //             console.log('error');
+  //         });
+  // });
+})
