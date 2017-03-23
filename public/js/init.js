263e441832cdbbc1d538d842d9b52d284f95970d
BRAD FOLKERS <BRADLEYDFOLKERS@GMAIL.COM>
2017-03-23 14:43:05 -0600

    removed jquery stuff from init.js

--- public/js/init.js
+++ public/js/init.js
@@ -1,13 +1,3 @@
-$.ajaxSetup({xhrFields: { withCredentials: true } });
-$.get('https://rhinocards.herokuapp.com/isLoggedIn')
-  .then(function(isLoggedIn) {
-    if (!isLoggedIn) {
-      window.location.href = 'https://flashrhino.com';
-    }
-  })
-  .catch(function(error) {
-    console.log(error);
-  })
 (function($){
   $(function(){
 
